4852c7364dc0b30afc6e5f275ce3f155d141031a
Roney <ronaldorhoney@hotmail.com>
2024-02-26 15:10:26 -0300

    arquivos desnecessários, excluídos

---
+++ dominando_strings_e_fatiamentos/conhecendometodosdasclasses.py
@@ -0,0 +1,29 @@
+# MAIÚSCOLO, munúscolo e Título #
+
+curso = " pYtHoN"
+
+print(curso.upper()) #  convertte para MAIÚSCULO #
+
+print(curso.lower()) # converte para minuúsculo #
+
+print(curso.title()) # apenas a primeira letra em MAIÚSCOLO #
+
+# Eliminando espaços em branco #
+
+curso = "    Python "
+
+print(curso.strip()) #Remove todos os espaços#
+
+print(curso.lstrip()) #remove os espaços da esquerda#
+
+print(curso.rstrip()) #remove os espaços da direita#
+
+"""Junções e centralização"""
+
+curso = "Python"
+
+print(curso.center(20, "*"))
+
+print(".".join(curso))
+
+
