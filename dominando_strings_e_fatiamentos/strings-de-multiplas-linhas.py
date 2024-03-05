31792adf3192e78df803bc9a0dd16ea524d99561
Roney <ronaldorhoney@hotmail.com>
2024-03-05 10:34:34 -0300

    strings multiplas linhas

---
+++ dominando_strings_e_fatiamentos/strings-de-multiplas-linhas.py
@@ -0,0 +1,22 @@
+nome = "Rhoney"
+
+mensagem = f"""
+    Olá meu nome é {nome}, 
+eu estou aprendendo Python.nome
+            Essa mensagem preserva a identação do texto. 
+"""
+
+print(mensagem)
+
+print(
+    """
+    ***************MENU**************
+    1 - Depositar o dinheiro
+    2 - Verificar o saldo
+    3 - Sacar
+    0 - Sair do sistema
+
+    *********************************
+            Obrigado por usar nossos serviços !!
+"""
+)    
